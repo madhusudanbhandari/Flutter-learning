68fee2fcfc2936f9f42aff5cf4fb6021b41a9b56
Madhusudan Bhandari <madhusudanb636@gmail.com>
2026-04-10 20:23:03 +0545

    3.stateless widget

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,21 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Hello", style: TextStyle(fontSize: 20.0)),
-          centerTitle: true,
-          backgroundColor: Colors.red,
-        ),
-        body: Center(child: Text("HIII")),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Text("Click"),
-          backgroundColor: Colors.red,
-        ),
+  runApp(MyWidget());
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hello", style: TextStyle(fontSize: 20.0)),
+        centerTitle: true,
+        backgroundColor: Colors.red,
       ),
-    ),
-  );
+      body: Center(child: Text("HIII")),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text("Click"),
+        backgroundColor: Colors.red,
+      ),
+    );
+  }
 }

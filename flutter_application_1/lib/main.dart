5d4cccb9f2c6d7dc3b31ba8b3be4c9def3bc913a
Madhusudan Bhandari <madhusudanb636@gmail.com>
2026-04-11 08:09:13 +0545

    6.Container

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -19,8 +19,15 @@ class MyWidget extends StatelessWidget {
       //body: Center(child: Text("HIII")),
       //body: Center(child: Image(image: AssetImage("assets/one.png"))),
       //body: Center(child: Icon(Icons.add_ic_call)),
-      body: Center(
-        child: ElevatedButton(onPressed: () {}, child: Text("click")),
+      // body: Center(
+      //   child: ElevatedButton(onPressed: () {},
+      //    child: Text("click"),),
+      // ),
+      body: Container(
+        padding: EdgeInsets.all(80.0),
+        margin: EdgeInsets.all(80),
+        color: Colors.grey,
+        child: Text("Hello"),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
